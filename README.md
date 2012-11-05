06612bf142eb3abee6f3207fb781dfcc49d0e042
Piotr Domagalski <piotr@domagalski.com>
2012-11-05 12:54:14 +0100

    Initial commit of a working version.

---
+++ README.md
@@ -0,0 +1,4 @@
+ffsqueue - Fast Fixed Size Queue
+================================
+
+Clean and fast implementation of a fixed size queue in JavaScript.
---
+++ ffsqueue.js
@@ -0,0 +1,62 @@
+"use strict";
+
+function FastFixedSizeQueue(size) {
+	if (size < 0) {
+		throw Error('Invalid queue size');
+	}
+	this.headIndex = 0;
+	this.queueLength = 0;
+	this.queueSize = size;
+	this.data = new Array(this.queueSize);
+}
+
+FastFixedSizeQueue.prototype = {
+	enqueue: function (value) {
+		if (this.queueLength >= this.queueSize) {
+			return -1;
+		}
+
+		this.data[(this.headIndex + this.queueLength) % this.queueSize] = value;
+		this.queueLength += 1;
+
+		return this.queueLength;
+	},
+
+	dequeue: function () {
+		var result;
+
+		if (this.queueLength <= 0) {
+			return undefined;
+		}
+
+		result = this.data[this.headIndex];
+		this.data[this.headIndex] = undefined;
+
+		this.headIndex = (this.headIndex + 1) % this.queueSize;
+		this.queueLength -= 1;
+
+		return result;
+	},
+
+	peek: function () {
+		if (this.queueLength <= 0) {
+			return undefined;
+		}
+
+		return this.data[this.headIndex];
+	},
+
+	get length() {
+		return this.queueLength;
+	},
+
+	get size() {
+		return this.queueSize;
+	},
+
+	clear: function () {
+		this.headIndex = 0;
+		this.queueLength = 0;
+		this.data = new Array(this.queueSize);
+	}
+};
